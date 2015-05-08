747127e9dc9e1716139a2ec8dd864b927d27da5c
Edwin <edwin.cubillos@udea.edu.co>
2015-05-07 23:03:09 -0500

    Version 3.0 Fragment Dinámico: Carga los dos Fragments Flash y Superman por medio de un menú

--- app/src/main/java/com/example/exfragments/MainActivity.java
+++ app/src/main/java/com/example/exfragments/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends ActionBarActivity {
         fragmentTransaction.add(android.R.id.content, fragment).commit();
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -32,14 +31,18 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (id == R.id.menu_flash) {
+            Flash fragment = new Flash();
+            fragmentTransaction.replace(android.R.id.content, fragment).commit();
+        }
+        if (id == R.id.menu_superman) {
+            Superman fragment = new Superman();
+            fragmentTransaction.replace(android.R.id.content, fragment).commit();
         }
 
         return super.onOptionsItemSelected(item);
