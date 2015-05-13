1ce9632c49fec144292ddc39f5d39b45f1370df2
Edwin <edwin.cubillos@udea.edu.co>
2015-05-13 07:12:04 -0500

    Version 6.0: Fragment Deslizables con un ViewPager, 3 Fragments: Superman, Flash y la Mujer Maravilla, la versión 5.0 es con Layouts deslizables que pueden ser actividades

--- app/src/main/java/com/example/exfragments/MainActivity.java
+++ app/src/main/java/com/example/exfragments/MainActivity.java
@@ -1,83 +1,35 @@
 package com.example.exfragments;
 
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
-import android.widget.Toast;
+import java.util.ArrayList;
 
-public class MainActivity extends ActionBarActivity {
-
-    private ViewPager viewPager;
-
-    private LinearLayout page1;
-    private LinearLayout page2;
-    private LinearLayout page3;
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager) findViewById(R.id.pager);
 
-        viewPager.setAdapter(new MyPageAdapter());
+        Flash FragFlash = new Flash();
+        Superman FragSuperman = new Superman();
+        MujerMaravilla FragMujerMaravilla = new MujerMaravilla();
+        ArrayList<Fragment> fragmentList = new ArrayList<Fragment>();
 
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() { //Se genera automaticamente los 3 metodos
-            @Override
-            public void onPageScrolled(int i, float v, int i2) {
-            }
+        fragmentList.add(FragFlash);
+        fragmentList.add(FragSuperman);
+        fragmentList.add(FragMujerMaravilla);
 
-            @Override
-            public void onPageSelected(int i) {
-                Toast.makeText(MainActivity.this, "page " + (i + 1), Toast.LENGTH_SHORT).show();
-            }
+        MyPagerAdapter mSectionsPagerAdapter = new MyPagerAdapter(getSupportFragmentManager(), fragmentList);
 
-            @Override
-            public void onPageScrollStateChanged(int i) {
-            }
-        });
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
     }
-
-        class MyPageAdapter extends PagerAdapter{
-
-            public int getCount() {
-                return 3;
-            }
-            public Object instantiateItem(ViewGroup container, int position) {
-                View page = null;
-                switch (position){
-                    case 0: if (page1==null)
-                                page1=(LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout.fragment_flash,null);
-                            page=page1;
-                            break;
-                    case 1: if (page2==null)
-                                page2=(LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout.fragment_superman,null);
-                            page=page2;
-                            break;
-                    default: if (page3==null)
-                                page3=(LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout.fragment_mujer_maravilla,null);
-                            page=page3;
-                            break;
-                }
-                ((ViewPager) container).addView(page,0);
-                return page;
-            }
-
-            public boolean isViewFromObject(View view, Object o) {
-                return view==o;
-            }
-
-            public void destroyItem(ViewGroup container, int position, Object object) {
-                ((ViewPager) container).removeView((View) object);
-            }
-        }
 
 
        /* FragmentManager fragmentManager = getFragmentManager();
